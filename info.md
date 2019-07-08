58688e6498155e2270a72515e12eb46cca1e9edd
Maron martell Mraz <steklyanov@gmail.com>
2019-07-08 19:48:27 +0300

    rebuild

--- info.md
+++
@@ -1,3 +0,0 @@
-### Makefile
-
-    http://rus-linux.net/nlib.php?name=/MyLDP/algol/gnu_make/gnu_make_3-79_russian_manual.html
--- printf.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINTF_H
-# define PRINTF_H
-
-#include <stdarg.h>
-#include <unistd.h>
-
-int ft_printf(const char *format, ...);
-
-#endif
